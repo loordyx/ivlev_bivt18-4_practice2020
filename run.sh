4528c46ce1282c85ad631e90f306c5a79eabdafa
loordyx <59841616+loordyx@users.noreply.github.com>
2020-07-14 09:45:16 +0300

    Delete run.sh

--- run.sh
+++
@@ -1,6 +0,0 @@
- $ cat ./run.sh
- #!/bin/bash
- # generates html from jupyter notebook
- jupyter-nbconvert --execute ./ml.ipynb
- # opens generated file in the browser (or whatever it can open it with)
- xdg-open ./ml.html
